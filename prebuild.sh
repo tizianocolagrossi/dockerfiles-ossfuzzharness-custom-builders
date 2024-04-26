bf5fbe9436f2ea7fec51061f97d1c9a7f5206d58
Tiziano Colagrossi <tiziano.colagrossi@gmail.com>
2024-04-26 12:24:23 +0000

    image name changed

--- prebuild.sh
+++ prebuild.sh
@@ -5,4 +5,4 @@ docker build -t oss/base-clang ./base-clang
 
 DOCKER_BUILDKIT=1 docker build -t builder/libafl-baseline --ssh default=$HOME/.ssh/id_ed25519 ./builder-libafl-baseline
 DOCKER_BUILDKIT=1 docker build -t builder/libafl-enumetric --ssh default=$HOME/.ssh/id_ed25519 ./builder-libafl-enumetric
-DOCKER_BUILDKIT=1 docker build -t builder/libafl-enumetricMM --ssh default=$HOME/.ssh/id_ed25519 ./builder-libafl-enumetricMM
+DOCKER_BUILDKIT=1 docker build -t builder/libafl-enumetric_mm --ssh default=$HOME/.ssh/id_ed25519 ./builder-libafl-enumetricMM
