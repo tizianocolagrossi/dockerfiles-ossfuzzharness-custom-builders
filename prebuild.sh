acec9a5655140f359f02ad8e4783cb021267f505
Tiziano Colagrossi <tiziano.colagrossi@gmail.com>
2024-04-26 17:02:36 +0000

    build images script

--- prebuild.sh
+++ prebuild.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
 
-docker build -t oss/base-image ./base-image
-docker build -t oss/base-clang ./base-clang
+docker build -t oss/base-image   ./base-image
+docker build -t oss/base-clang   ./base-clang
+docker build -t oss//base-runner ./base-runner
 
-DOCKER_BUILDKIT=1 docker build -t builder/libafl-baseline --ssh default=$HOME/.ssh/id_ed25519 ./builder-libafl-baseline
-DOCKER_BUILDKIT=1 docker build -t builder/libafl-enumetric --ssh default=$HOME/.ssh/id_ed25519 ./builder-libafl-enumetric
+DOCKER_BUILDKIT=1 docker build -t builder/libafl-baseline     --ssh default=$HOME/.ssh/id_ed25519 ./builder-libafl-baseline
+DOCKER_BUILDKIT=1 docker build -t builder/libafl-enumetric    --ssh default=$HOME/.ssh/id_ed25519 ./builder-libafl-enumetric
 DOCKER_BUILDKIT=1 docker build -t builder/libafl-enumetric_mm --ssh default=$HOME/.ssh/id_ed25519 ./builder-libafl-enumetricMM
