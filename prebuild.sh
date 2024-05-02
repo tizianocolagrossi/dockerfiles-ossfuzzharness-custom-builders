58ff3a3d4d8b0fef1f1638e2e4ec25e261e0bb60
Tiziano Colagrossi <tiziano.colagrossi@gmail.com>
2024-05-02 19:21:47 +0200

    script to init docker images requested

--- prebuild.sh
+++ prebuild.sh
@@ -1,9 +1,31 @@
 #! /bin/bash
 
-docker build -t oss/base-image   ./base-image
-docker build -t oss/base-clang   ./base-clang
-docker build -t oss//base-runner ./base-runner
+if [ -z "$1" ]
+  then
+    echo "Version of enumetric to build required"
+    echo "Example: ./$0 v0.2.4"
+    exit 0
+fi
 
-DOCKER_BUILDKIT=1 docker build -t builder/libafl-baseline     --ssh default=$HOME/.ssh/id_ed25519 ./builder-libafl-baseline
-DOCKER_BUILDKIT=1 docker build -t builder/libafl-enumetric    --ssh default=$HOME/.ssh/id_ed25519 ./builder-libafl-enumetric
-DOCKER_BUILDKIT=1 docker build -t builder/libafl-enumetric_mm --ssh default=$HOME/.ssh/id_ed25519 ./builder-libafl-enumetricMM
+
+docker build -t oss/base-image  ./base-image
+docker build -t oss/base-clang  ./base-clang
+docker build -t oss/base-runner ./base-runner
+
+DOCKER_BUILDKIT=1 docker build \
+    --build-arg enumetric-version=$1 \
+    -t builder/libafl-baseline:$1 \
+    --ssh default=$HOME/.ssh/id_ed25519 \
+    ./builder-libafl-baseline
+
+DOCKER_BUILDKIT=1 docker build \
+    --build-arg enumetric-version=$1 \
+    -t builder/libafl-enumetric:$1 \
+    --ssh default=$HOME/.ssh/id_ed25519 \
+    ./builder-libafl-enumetric
+
+DOCKER_BUILDKIT=1 docker build \
+    --build-arg enumetric-version=$1 \
+    -t builder/libafl-enumetric_mm:$1 \
+    --ssh default=$HOME/.ssh/id_ed25519 \
+    ./builder-libafl-enumetricMM
