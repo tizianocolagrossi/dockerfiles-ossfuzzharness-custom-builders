4aca253ed1799f56204926be267925fe35ba3180
Tiziano Colagrossi <tiziano.colagrossi@gmail.com>
2024-05-06 13:28:00 +0200

    update with nre base builder

--- prebuild.sh
+++ prebuild.sh
@@ -3,7 +3,7 @@
 if [ -z "$1" ]
   then
     echo "Version of enumetric to build required"
-    echo "Example: ./$0 v0.2.4"
+    echo "Example: $0 v0.2.4"
     exit 0
 fi
 
@@ -11,22 +11,10 @@ fi
 docker build -t oss-base-image  ./base-image
 docker build -t oss-base-clang  ./base-clang
 docker build -t oss-base-runner ./base-runner
-docker build -t enumetric-analysis:$1 ./analysis-libafl-enumetric
+# docker build -t enumetric-analysis:$1 ./analysis-libafl-enumetric
 
 DOCKER_BUILDKIT=1 docker build \
     --build-arg enumetric-version=$1 \
-    -t builder-libafl-baseline:$1 \
+    -t base-builder:$1 \
     --ssh default=$HOME/.ssh/id_ed25519 \
-    ./builder-libafl-baseline
-
-DOCKER_BUILDKIT=1 docker build \
-    --build-arg enumetric-version=$1 \
-    -t builder-libafl-enumetric:$1 \
-    --ssh default=$HOME/.ssh/id_ed25519 \
-    ./builder-libafl-enumetric
-
-DOCKER_BUILDKIT=1 docker build \
-    --build-arg enumetric-version=$1 \
-    -t builder-libafl-enumetric_mm:$1 \
-    --ssh default=$HOME/.ssh/id_ed25519 \
-    ./builder-libafl-enumetricMM
+    ./base-builder
