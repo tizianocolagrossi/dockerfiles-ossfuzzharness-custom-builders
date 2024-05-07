872c0273c1fe2e08861d97b7b230ccbaeceb6fe3
Tiziano Colagrossi <tiziano.colagrossi@gmail.com>
2024-05-07 11:26:44 +0200

    add libxml 2.9.2 according to fuzzbench

---
+++ sut-images/libxml2-v2.9.2/build.sh
@@ -0,0 +1,42 @@
+#!/bin/bash -eu
+#
+# Copyright 2016 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+################################################################################
+
+# fuzz/oss-fuzz-build.sh
+
+export PKG_CONFIG="`which pkg-config` --static"
+# export PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig
+
+pushd $SRC/libxml2
+
+rm -rf BUILD
+cp -rf . $SRC/BUILD
+mv $SRC/BUILD .
+
+cd BUILD 
+./autogen.sh
+ CCLD="$CXX $CXXFLAGS" ./configure --disable-shared
+ make -j$(nproc)
+
+set -x
+$CXX $CXXFLAGS -std=c++11  $SRC/target.cc -I $SRC/libxml2/BUILD/include $SRC/libxml2/BUILD/.libs/libxml2.a -lz -llzma --libafl -o $OUT/xml
+
+mkdir $SRC/corpus
+echo "hi" > $SRC/corpus/seed
+zip -j $OUT/xml.zip $SRC/corpus/*
+
+popd
