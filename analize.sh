44d787b7558ab4260aa067d2fa363693d7a3ce52
Tiziano Colagrossi <tiziano.colagrossi@gmail.com>
2024-05-21 17:27:04 +0200

    add dedup build in helper

--- analize.sh
+++ analize.sh
@@ -2,9 +2,9 @@
 
 # usage analyze.sh fuzzer_campaign_out fuzzer_analysis_build
 
-if [ "$#" -ne 2 ]
+if [ "$#" -ne 3 ]
 then
-  echo "USAGE: $0 <fuzzer-out-dir> <fuzzer-analysis-build>"
+  echo "USAGE: $0 <fuzzer-out-dir> <fuzzer-analysis-build> <fuzzer-deduplication-build-w-sanitizers>"
   exit 1
 fi
 
@@ -12,8 +12,17 @@ FUZZ_OUT=$1
 
 FUZZER_BUILD_ANALYSIS=$(realpath $2)
 
+FUZZER_BUILD_DEDUP=$(realpath $3)
+
 
 FUZZER=$(basename $FUZZER_BUILD_ANALYSIS)
 ANALYSIS_BUILD=$(dirname $FUZZER_BUILD_ANALYSIS)
+FUZZER_DEDUP=$(basename $FUZZER_BUILD_DEDUP)
+DEDUP_BUILD=$(dirname $FUZZER_BUILD_DEDUP)
 
-docker run -it --rm -v $FUZZ_OUT:/fuzz_out -v $ANALYSIS_BUILD:/out -e FUZZING_ENGINE=libafl -t oss-base-analysis analyze $FUZZER
+if [ $FUZZER_DEDUP -ne $FUZZER ] ; then 
+  echo "Error fuzzer analysis and deduplication are different!"
+  exit 1
+fi
+
+docker run -it --rm -v $FUZZ_OUT:/fuzz_out $DEDUP_BUILD:/dedup_build -v $ANALYSIS_BUILD:/out -e FUZZING_ENGINE=libafl -t oss-base-analysis analyze $FUZZER
