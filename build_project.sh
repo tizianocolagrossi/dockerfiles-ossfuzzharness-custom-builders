5b3ed30e8d11913ee5fdbfd32fbefd23eb2132cc
Tiziano Colagrossi <tiziano.colagrossi@gmail.com>
2024-05-23 12:18:35 +0200

    build add single project

---
+++ build_project.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+if [ "$#" -ne 1 ]
+then
+  echo "USAGE: $0 <project-path>"
+  exit 1
+fi
+
+
+project_path=$(realpath $1)
+echo $project_path
+project_name=$(basename $project_path)
+echo $project_name
+
+docker build -t fuzzbuild/$project_name $project_path/
+
+for type in analysis baseline enumetric enumetric++ ; do
+    cmd=compile_$type
+    echo $type
+    echo $cmd
+    build_dir=$HOME/sut-docker/$project_name/$type/
+    if [ -d "$build_dir" ]; then
+        echo "$build_dir does exist."
+        continue
+    fi
+    mkdir -p $build_dir
+    docker run -it --rm -v $build_dir:/out -t fuzzbuild/$project_name $cmd
+done 
