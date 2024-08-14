9eaa37fabe01437f91a091fd95a72cf32f2631df
Tiziano Colagrossi <tiziano.colagrossi@gmail.com>
2024-08-14 13:09:06 +0200

    change naming convenction

--- prebuild.sh
+++ prebuild.sh
@@ -7,26 +7,27 @@ if [ -z "$1" ]
     exit 0
 fi
 
+SSH_CREDS=$HOME/.ssh/id_ed25519
 
-docker build -t oss-base-image  ./base-image
-docker build -t oss-base-clang  ./base-clang
-docker build -t oss-base-runner ./base-runner
+docker build -t osvaldo/oss-base-image  ./base-image
+docker build -t osvaldo/oss-base-clang  ./base-clang
+docker build -t osvaldo/oss-base-runner ./base-runner
 
 # docker build -t oss-base-analysis ./base-analysis
 DOCKER_BUILDKIT=1 docker build \
-    -t oss-base-analysis \
-    --ssh default=$HOME/.ssh/id_ed25519 \
+    -t osvaldo/oss-base-analysis \
+    --ssh default=$SSH_CREDS \
     ./base-analysis
 
 # DOCKER_BUILDKIT=1 docker build \
 #     --build-arg UID=$(id -u) --build-arg GID=$(id -g) \
 #     -t oss-base-analysis-crash \
-#     --ssh default=$HOME/.ssh/id_ed25519 \
+#     --ssh default=$SSH_CREDS \
 #     ./base-analysis-crash
 # docker build -t enumetric-analysis:$1 ./analysis-libafl-enumetric
 
 DOCKER_BUILDKIT=1 docker build \
     --build-arg enumetricversion=$1 \
-    -t base-builder:$1 \
-    --ssh default=$HOME/.ssh/id_ed25519 \
+    -t osvaldo/base-builder:$1 \
+    --ssh default=$SSH_CREDS \
     ./base-builder
