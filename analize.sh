102b1943565e84a792cad6144a5f13c12f028d31
Tiziano Colagrossi <tiziano.colagrossi@gmail.com>
2024-10-07 17:06:45 +0200

    add option for coverage profiling

--- analize.sh
+++ analize.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 # usage analyze.sh fuzzer_campaign_out fuzzer_analysis_build
-
+coverage_profile_enabled=false
 if [ "$#" -ne 3 ]
 then
   echo "USAGE: $0 <fuzzer-out-dir> <fuzzer-analysis-build> <fuzzer-deduplication-build-w-sanitizers>"
@@ -25,8 +25,9 @@ if [ $FUZZER_DEDUP -ne $FUZZER ] ; then
   exit 1
 fi
 
-docker run -it --rm --shm-size=1gb -v $FUZZ_OUT:/fuzz_out -v $DEDUP_BUILD:/dedup_build -v $ANALYSIS_BUILD:/out -e FUZZ_OUT_REAL_PATH=$FUZZ_OUT -e OUTUID=$(id -u) -e OUTGID=$(id -g) -t osvaldo/oss-base-analysis analyze $FUZZER
+# docker run -it --rm --shm-size=1gb -v $FUZZ_OUT:/fuzz_out -v $DEDUP_BUILD:/dedup_build -v $ANALYSIS_BUILD:/out -e FUZZ_OUT_REAL_PATH=$FUZZ_OUT -e OUTUID=$(id -u) -e OUTGID=$(id -g) -t osvaldo/oss-base-analysis analyze $FUZZER
 
+docker run -it --rm --shm-size=1gb -v $FUZZ_OUT:/fuzz_out -v $DEDUP_BUILD:/dedup_build -v $ANALYSIS_BUILD:/out -e FUZZ_OUT_REAL_PATH=$FUZZ_OUT -e COVERAGE_PROFILE=$coverage_profile_enabled -e OUTUID=$(id -u) -e OUTGID=$(id -g) -t osvaldo/oss-base-analysis analyze $FUZZER
 # for d in /home/tiziano/Documents/new-docker-run/new_evluation-v0.2.6tmp3/xpdf-v4.00/regression_v4.00/* ; do ./analize.sh $d /home/tiziano/sut-docker/v0.2.6tmp6/xpdf-v4.00/analysis/fuzz_pdfload /home/tiziano/sut-docker/v0.2.6tmp6/xpdf-v4.00/enumetricbb++/fuzz_pdfload ; done;
 # for d in /home/tiziano/Documents/new-docker-run/new_evluation-v0.2.6tmp3/xpdf-v4.00/regression_v4.01/* ; do ./analize.sh $d /home/tiziano/sut-docker/v0.2.6tmp6/xpdf-v4.01/analysis/fuzz_pdfload /home/tiziano/sut-docker/v0.2.6tmp6/xpdf-v4.01/enumetricbb++/fuzz_pdfload ; done;
 # for d in /home/tiziano/Documents/new-docker-run/new_evluation-v0.2.6tmp3/xpdf-v4.00/regression_v4.02/* ; do ./analize.sh $d /home/tiziano/sut-docker/v0.2.6tmp6/xpdf-v4.02/analysis/fuzz_pdfload /home/tiziano/sut-docker/v0.2.6tmp6/xpdf-v4.02/enumetricbb++/fuzz_pdfload ; done;
