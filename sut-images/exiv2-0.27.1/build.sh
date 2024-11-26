bedd2fa9e971952664bc2c52752208b767422072
Tiziano Colagrossi <tiziano.colagrossi@gmail.com>
2024-11-26 18:21:16 +0100

    build exiv0271

--- sut-images/exiv2-0.27.1/build.sh
+++ sut-images/exiv2-0.27.1/build.sh
@@ -3,8 +3,8 @@ cd $SRC/exiv2
 mkdir build
 cd build
 
-cmake .. -G "Unix Makefiles"
-cmake --build .
+cmake -G "Unix Makefiles" -DBUILD_SHARED_LIBS=Off ..
+make -j $(nproc)
 
 cp bin/* /out/
 
@@ -12,3 +12,21 @@ ldd bin/exiv2
 
 cd ..
 rm -rf build 
+
+export AFL_LLVM_CMPLOG=1
+
+cd $SRC/exiv2
+mkdir build
+cd build
+
+cmake -G "Unix Makefiles" -DBUILD_SHARED_LIBS=Off ..
+make -j $(nproc)
+
+cp bin/exiv2 /out/exiv2.cmplog
+
+cd ..
+rm -rf build 
+
+touch $OUT/afl_cmplog.txt
+unset AFL_LLVM_CMPLOG
+
