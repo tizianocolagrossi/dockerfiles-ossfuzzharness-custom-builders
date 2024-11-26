0b906f5f6e49720d58a29069f905c4e40ab382e2
Tiziano Colagrossi <tiziano.colagrossi@gmail.com>
2024-11-26 18:21:33 +0100

    edit run fuzzer for fork mode

--- prebuild_runner.sh
+++ prebuild_runner.sh
@@ -4,4 +4,4 @@ SSH_CREDS=$HOME/.ssh/id_ed25519
 
 docker build -t osvaldo/oss-base-image  ./docker-images/base-image
 docker build -t osvaldo/oss-base-clang  ./docker-images/base-clang
-docker build -t osvaldo/oss-base-runner ./docker-images/base-runner
+docker build --no-cache -t osvaldo/oss-base-runner ./docker-images/base-runner
