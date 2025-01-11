b28ce1e9b24f866d8d645b5a54629400cd35d85c
Tiziano Colagrossi <tiziano.colagrossi@gmail.com>
2025-01-11 11:11:30 +0100

    removed unused option

--- prebuild.sh
+++ prebuild.sh
@@ -1,11 +1,5 @@
 #! /bin/bash
 
-if [ -z "$1" ]
-  then
-    echo "Version of enumetric to build required"
-    echo "Example: $0 v0.2.6tmp6-aflpp-doublem"
-    exit 0
-fi
 
 SSH_CREDS=$HOME/.ssh/id_ed25519
 
@@ -21,7 +15,6 @@ DOCKER_BUILDKIT=1 docker build \
     ./docker-images/base-analysis
 
 DOCKER_BUILDKIT=1 docker build \
-    --build-arg enumetricversion=$1 \
-    -t osvaldo/base-builder:$1 \
+    -t osvaldo/base-builder \
     --ssh default=$SSH_CREDS \
     ./docker-images/base-builder
