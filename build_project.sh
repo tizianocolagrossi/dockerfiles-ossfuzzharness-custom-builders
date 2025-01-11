c95c7091d0faf873730944756c4f0cd2ec5cba18
Tiziano Colagrossi <tiziano.colagrossi@gmail.com>
2025-01-11 12:57:18 +0100

    moved old compiler scripts and update docker image and build_project

--- build_project.sh
+++ build_project.sh
@@ -1,25 +1,66 @@
 #! /bin/bash
 
-if [ "$#" -eq 0 ]
-then
-  echo "USAGE: $0 <project-path> <analysis baseline enumetric enumetric++ enumetricbb++ aflpp aflppdouble_baseline aflppdouble_enumetric aflppdouble_enumetric++ aflppdouble_enumetricbb++ aflppdouble_enumetric_full>"
-  exit 1
+# Usage function
+usage() {
+    echo "Usage: $0 -c <compiler> -s <sanitizers> -f <fuzzing_mode> -b <build_type> <project_path>"
+    echo "  -c    Compiler (e.g., aflpp, aflppdouble, clang, auto) default is auto ans is used for multiple instrumentations"
+    echo "  -s    Sanitizers (e.g., asan, ubsan, coverage, enumcoverage, debug)"
+    echo "  -f    Fuzzing mode (e.g. fork, persistent)"
+    echo "  -b    Builds (e.g., aflpp, enumetric, enumetric++, enumetricbb++, enumetric_full ) will be also the name of the directory"
+    echo "  <project_path> Path to the project to build"
+    exit 1
+}
+
+# Get the current user's UID
+current_uid=$(id -u)
+
+# Default values
+compiler="auto"
+sanitizers="asan ubsan"
+fuzzing_mode="persistent"
+builds=""
+project_path=""
+
+# Parse options
+while getopts "c:s:f:b:" opt; do
+    case $opt in
+        c) compiler="$OPTARG" ;;
+        s) sanitizers="$OPTARG" ;;
+        f) fuzzing_mode="$OPTARG" ;;
+        b) builds="$OPTARG" ;;
+        *) usage ;;
+    esac
+done
+
+# Shift processed options
+shift $((OPTIND - 1))
+
+# Remaining argument is the project path
+if [[ $# -eq 1 ]]; then
+    project_path="$1"
+else
+    echo "Error: You must specify a project path."
+    usage
 fi
 
-if [ "$#" -eq 1 ]
-then
-  echo "USAGE: $0 $1 <analysis baseline enumetric enumetric++ enumetricbb++ aflpp aflppdouble_baseline aflppdouble_enumetric aflppdouble_enumetric++ aflppdouble_enumetricbb++ aflppdouble_enumetric_full>"
-  exit 1
+# Validate inputs
+if [[ -z "$compiler" || -z "$sanitizers" || -z "$fuzzing_mode" ]]; then
+    echo "Error: All options (-c, -s, -f, -i) are required."
+    usage
 fi
 
+if [[ ! -d "$project_path" ]]; then
+    echo "Error: The specified project path does not exist or is not a directory: $project_path"
+    exit 1
+fi
 
-project_path=$(realpath $1)
+project_path=$(realpath $project_path)
 echo $project_path
 project_name=$(basename $project_path)
 echo $project_name
 
-# docker build --no-cache -t osvaldo/$project_name $project_path/ 
-docker build -t osvaldo/$project_name $project_path/ 
+docker build -t osvaldo/$project_name $project_path/  #--no-cache
+# docker build -t osvaldo/$project_name $project_path/ 
 if [ $? -eq 0 ]; then
     echo "Image created successfully."
 else
@@ -27,18 +68,49 @@ else
     exit 1
 fi
 
-shift
 
-for type in  $@ ; do # analysis baseline enumetric enumetric++ enumetricbb++ aflpp aflpp_manual aflppdouble_baseline aflppdouble_enumetric aflppdouble_enumetric++ aflppdouble_enumetricbb++ aflppdouble_enumetric_full
-    cmd=compile_$type
-    echo $type
-    echo $cmd
-    build_dir=$HOME/sut-docker/$project_name/$type/
-    if [ -d "$build_dir" ]; then
-        echo "$build_dir does exist."
-        continue
+for build in $builds; do
+  compiler_chosed=""
+  if [ "$compiler" == "auto" ] ; then 
+    if [ "$build" == "aflpp" ] ; then
+      compiler_chosed="aflpp"
     fi
-    mkdir -p $build_dir
-    # docker run -it --rm -v $build_dir:/out -t osvaldo/$project_name /bin/bash
-    docker run -it --rm -v $build_dir:/out -t osvaldo/$project_name $cmd
-done 
+    if [ "$build" == "enumetric" ] || \
+    [ "$build" == "enumetric++" ] || \
+    [ "$build" == "enumetricbb++" ] || \
+    [ "$build" == "enumetric_full" ]  ; then
+      compiler_chosed="aflppdouble"
+    fi
+  else
+    compiler_chosed=$compiler
+  fi
+
+
+  # Display selected options
+  echo "Compiler: $compiler_chosed"
+  echo "Sanitizers: $sanitizers"
+  echo "Fuzzing Mode: $fuzzing_mode"
+  echo "Build: $build"
+  echo "Project Path: $project_path"
+  echo ""
+
+  shift
+
+  build_dir=$HOME/sut-docker/$project_name/$build/
+  if [ -d "$build_dir" ]; then
+      echo "$build_dir does exist."
+      exit 1
+  fi
+  
+  mkdir -p $build_dir
+
+  docker run -it --rm \
+    --env SANITIZERS="$sanitizers" \
+    --env FUZZINGMODE="$fuzzing_mode" \
+    --env COMPILER="$compiler_chosed" \
+    --env BUILDTYPE="$build" \
+    --env BUILD_UID=$current_uid \
+    -v $build_dir:/out -t osvaldo/$project_name
+
+done
+
