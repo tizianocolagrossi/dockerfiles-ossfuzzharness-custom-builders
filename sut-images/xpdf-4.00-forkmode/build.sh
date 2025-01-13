20e4825c24870b7e62b1b21d22b8170fc9078da7
Tiziano Colagrossi <tiziano.colagrossi@gmail.com>
2025-01-13 15:28:49 +0100

    add xpdf 4.00 forkmode like unibench

---
+++ sut-images/xpdf-4.00-forkmode/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -eu
+
+cd $SRC/xpdf-4.00
+mkdir build
+cd build
+cmake -DCMAKE_CXX_FLAGS="$CXXFLAGS" ..
+make -j
+find ./xpdf -type f -executable -exec cp "{}" /out/ \;
---
+++ sut-images/xpdf-4.00-forkmode_cmplog/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -eu
+export AFL_LLVM_CMPLOG=1
+cd $SRC/xpdf-4.00
+mkdir build
+cd build
+cmake -DCMAKE_CXX_FLAGS="$CXXFLAGS" ..
+make -j
+find ./xpdf -type f -executable -exec mv "{}" "{}".cmplog \;
+find ./xpdf -type f -executable -exec cp "{}" /out/ \;
+
+cd ..
+rm -rf build 
+
+touch $OUT/afl_cmplog.txt
+unset AFL_LLVM_CMPLOG
+
+mkdir build
+cd build
+cmake -DCMAKE_CXX_FLAGS="$CXXFLAGS" ..
+make -j
+find ./xpdf -type f -executable -exec cp "{}" /out/ \;
+
+/bin/bash
