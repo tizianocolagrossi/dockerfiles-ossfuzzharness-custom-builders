19ade79a3861d98aecda52d5a08dbbf607f80baf
Tiziano Colagrossi <tiziano.colagrossi@gmail.com>
2025-03-11 12:15:11 +0100

    update manual analysis image

--- docker-images/manual-analysis/install_deps.sh
+++ docker-images/manual-analysis/install_deps.sh
@@ -10,12 +10,21 @@ apt-get update && apt-get install -y \
     python3-setuptools \
     unzip \
     wget \
+    locales \
     zip --no-install-recommends
+
+
+export LC_ALL=en_US.UTF-8 
+export PYTHONIOENCODING=UTF-8
+
+localedef -i en_US -c -f UTF-8 -A /usr/share/locale/locale.alias en_US.UTF-8
+locale-gen "en_US.UTF-8"
+
 
 git clone https://github.com/pwndbg/pwndbg.git /src/pwdbg
 pushd /src/pwdbg
 
-git checkout tags/2024.08.29
+git checkout tags/2022.08.30
 ./setup.sh
 
 popd
