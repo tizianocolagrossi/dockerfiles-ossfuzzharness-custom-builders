0386925b2b80b56aad172d5f32db65c2d1d902dd
Tiziano Colagrossi <tiziano.colagrossi@gmail.com>
2025-03-12 15:13:38 +0100

    add xpdf_forkmodelike_sgfuzz

---
+++ sut-images/xpdf-4.00-forkmode_sgfuzz/build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -eu
+
+cd $SRC/xpdf-4.00
+
+# replace with harness for libfuzzer
+rm $SRC/xpdf-4.00/xpdf/pdftotext.cc
+mv $SRC/pdftotext.cc $SRC/xpdf-4.00/xpdf/pdftotext.cc
+
+rm $SRC/xpdf-4.00/xpdf/CMakeLists.txt
+mv $SRC/CMakeLists.txt $SRC/xpdf-4.00/xpdf/CMakeLists.txt
+
+echo "tag" > filter.txt
+python3 /src/SGFuzz/sanitizer/State_machine_instrument.py -b filter.txt .
+
+
+mkdir build
+cd build
+cmake -DCMAKE_CXX_FLAGS="$CXXFLAGS" ..
+make -j
+
+find ./xpdf -type f -executable -exec cp "{}" /out/ \;
