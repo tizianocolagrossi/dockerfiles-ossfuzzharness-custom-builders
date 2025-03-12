9c48f272380b5d34c1741bf1c7228992148eefad
Tiziano Colagrossi <tiziano.colagrossi@gmail.com>
2025-03-12 15:55:40 +0100

    rebuild all xpdf-4.00 persitent

---
+++ sut-images/xpdf-4.00-persitent_cmplog/build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -eu
+export AFL_LLVM_CMPLOG=1
+cd $SRC/xpdf-4.00
+
+#replace CMakelist with edited for esporting lib
+rm ./xpdf/CMakeLists.txt
+cp $SRC/CMakeLists.txt.xpdf ./xpdf/CMakeLists.txt
+cp $SRC/fuzz_pdfload.cc ./xpdf/fuzz_pdfload.cc
+
+mkdir build
+cd build
+cmake -DCMAKE_CXX_FLAGS="$CXXFLAGS" ..
+make -j
+find ./xpdf -type f -executable -exec mv "{}" "{}".cmplog \;
+find ./xpdf -type f -executable -exec cp "{}" /out/ \;
+
+cd ..
+rm -rf build 
+
+touch $OUT/afl_cmplog.txt
+unset AFL_LLVM_CMPLOG
+
+mkdir build
+cd build
+cmake -DCMAKE_CXX_FLAGS="$CXXFLAGS" ..
+make -j
+find ./xpdf -type f -executable -exec cp "{}" /out/ \;
