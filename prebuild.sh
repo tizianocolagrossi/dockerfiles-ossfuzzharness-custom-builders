8125aab81ee461f34213b939f5be2e74bb1bda77
Tiziano Colagrossi <tiziano.colagrossi@gmail.com>
2025-03-12 12:37:42 +0100

    add base image llvm 10 in prebuidl

--- prebuild.sh
+++ prebuild.sh
@@ -3,10 +3,11 @@
 
 SSH_CREDS=$HOME/.ssh/id_ed25519
 
-docker build -t osvaldo/oss-base-image  ./docker-images/base-image
-docker build -t osvaldo/oss-base-clang  ./docker-images/base-clang
-docker build -t osvaldo/oss-base-runner ./docker-images/base-runner
-docker build -t osvaldo/manual-analysis ./docker-images/manual-analysis/
+docker build -t osvaldo/oss-base-image     ./docker-images/base-image
+docker build -t osvaldo/oss-base-clang     ./docker-images/base-clang
+docker build -t osvaldo/oss-base-clang-10  ./docker-images/base-clang-10
+docker build -t osvaldo/oss-base-runner    ./docker-images/base-runner
+docker build -t osvaldo/manual-analysis    ./docker-images/manual-analysis/
 
 # docker build -t oss-base-analysis ./base-analysis
 DOCKER_BUILDKIT=1 docker build \
@@ -18,3 +19,5 @@ DOCKER_BUILDKIT=1 docker build \
     -t osvaldo/base-builder \
     --ssh default=$SSH_CREDS \
     ./docker-images/base-builder
+
+docker build -t osvaldo/sgfuzzer-builder ./docker-images/sgfuzzer-builder
